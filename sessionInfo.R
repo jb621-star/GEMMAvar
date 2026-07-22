18b9266ec0c70bb7e7f8721321959b0cb3f5030a
jb621-star <65730803+jb621-star@users.noreply.github.com>
2026-07-22 09:26:42 -0400

    Add sessionInfo output for R environment details

---
+++ sessionInfo.R
@@ -0,0 +1,29 @@
+sessionInfo()
+R version 4.2.2 (2022-10-31)
+Platform: x86_64-pc-linux-gnu (64-bit)
+Running under: Ubuntu 22.04.1 LTS
+
+Matrix products: default
+BLAS:   /usr/lib/x86_64-linux-gnu/openblas-pthread/libblas.so.3
+LAPACK: /usr/lib/x86_64-linux-gnu/openblas-pthread/libopenblasp-r0.3.20.so
+
+locale:
+ [1] LC_CTYPE=en_US.UTF-8       LC_NUMERIC=C               LC_TIME=en_US.UTF-8        LC_COLLATE=en_US.UTF-8    
+ [5] LC_MONETARY=en_US.UTF-8    LC_MESSAGES=en_US.UTF-8    LC_PAPER=en_US.UTF-8       LC_NAME=C                 
+ [9] LC_ADDRESS=C               LC_TELEPHONE=C             LC_MEASUREMENT=en_US.UTF-8 LC_IDENTIFICATION=C       
+
+attached base packages:
+[1] parallel  stats     graphics  grDevices utils     datasets  methods   base     
+
+other attached packages:
+ [1] data.table_1.18.2.1 lubridate_1.9.5     forcats_1.0.1       stringr_1.6.0       dplyr_1.2.1         purrr_1.2.2        
+ [7] readr_2.2.0         tidyr_1.3.2         tibble_3.3.1        ggplot2_4.0.2       tidyverse_2.0.0     edgeR_3.40.2       
+[13] limma_3.54.0       
+
+loaded via a namespace (and not attached):
+ [1] Rcpp_1.1.1-1.1     pillar_1.11.1      compiler_4.2.2     RColorBrewer_1.1-3 tools_4.2.2        timechange_0.4.0  
+ [7] lifecycle_1.0.5    gtable_0.3.6       lattice_0.20-45    pkgconfig_2.0.3    rlang_1.2.0        cli_3.6.6         
+[13] rstudioapi_0.14    ggrepel_0.9.8      withr_3.0.2        generics_0.1.4     vctrs_0.7.2        hms_1.1.4         
+[19] locfit_1.5-9.12    grid_4.2.2         tidyselect_1.2.1   glue_1.8.0         ggnewscale_0.5.2   R6_2.6.1          
+[25] pbapply_1.7-4      farver_2.1.2       tzdb_0.5.0         magrittr_2.0.5     scales_1.4.0       dichromat_2.0-0.1 
+[31] S7_0.2.1           stringi_1.8.7 
